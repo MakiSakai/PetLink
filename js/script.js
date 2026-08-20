bc279adab896cfa573e06654dc4d13a1ae881fab
MakiSakai <maki.sakai2001@gmail.com>
2026-08-20 11:12:55 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// sliderの設定
+$('.slider').slick({
+    autoplay: true, //自動で動く
+    autoplaySpeed: 2000, // 次の画像に移るまで2秒かかる
+    fade: true, //フェイドイン・フェイドアウト
+    infinite: true, //無限ループ
+    arrows: false //矢印なし
+});
